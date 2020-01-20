21205e0cccb650a4461f3a7b9409999b03268753
ueno <ueno@nayuta.co>
2020-01-20 13:47:46 +0900

    use default port

--- mystart.sh
+++ mystart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./run.sh --regtest --chains=lbtc --lbtcrpcuser=user3 --lbtcrpcpassword=password3 --lbtcrpcurl=http://127.0.0.1:19332 --lbtcnodeendpoint=127.0.0.1:19333 --bind=0.0.0.0:32838 --noauth=1 --verbose=1 
+./run.sh --regtest --chains=lbtc --lbtcrpcuser=user3 --lbtcrpcpassword=password3 --lbtcrpcurl=http://127.0.0.1:19332 --lbtcnodeendpoint=127.0.0.1:19333 --noauth=1 --verbose=1 
